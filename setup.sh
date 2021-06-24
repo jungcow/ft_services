76a00e9037ab8ae68411208cedb665c6f705aa1c
Jungwoo Kim <jungwkim@c8r8s1.42seoul.kr>
2021-06-24 15:35:55 +0900

    second commit

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 function ready_minikube ()
 {
 	minikube delete --all
-	export MINIKUBE_HOME=~
-#	export MINIKUBE_HOME=~/goinfre
+#	export MINIKUBE_HOME=~
+	export MINIKUBE_HOME=~/goinfre
 	minikube config set memory 2048
 	minikube config set disk-size 4096
 	minikube start --driver=virtualbox
@@ -22,7 +22,6 @@ function build_metallb()
 {
 	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
 	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
-#	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 	sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" ./srcs/yaml/metallb.yaml
 	kubectl apply -f ./srcs/yaml/metallb.yaml
 	sed -i '' "s/$MINIKUBE_IP/MINIKUBE_IP/g" ./srcs/yaml/metallb.yaml
@@ -30,21 +29,21 @@ function build_metallb()
 
 function build_images()
 {
+	docker build ./srcs/image/mysql -t mysql
 	docker build ./srcs/image/phpmyadmin -t phpmyadmin
 	docker build ./srcs/image/wordpress -t wordpress
-	docker build ./srcs/image/mysql -t mysql
 }
 
 function build_minikube()
 {
 	build_metallb
-	kubectl apply -f ./srcs/yaml/nginx.yaml
-	kubectl apply -f ./srcs/yaml/mysql.yaml
-	kubectl apply -f ./srcs/yaml/phpmyadmin.yaml
-	kubectl apply -f ./srcs/yaml/wordpress.yaml
+#	kubectl apply -f ./srcs/yaml/mysql.yaml
+#	kubectl apply -f ./srcs/yaml/nginx.yaml
+#	kubectl apply -f ./srcs/yaml/phpmyadmin.yaml
+#	kubectl apply -f ./srcs/yaml/wordpress.yaml
 }
 
 ready_minikube
-build_nginx
-build_images
+#build_nginx
+#build_images
 build_minikube
--- srcs/image/phpmyadmin/srcs/init.sh
+++ srcs/image/phpmyadmin/srcs/init.sh
@@ -2,23 +2,22 @@
 
 # Prepare
 mkdir -p /run/nginx
+mkdir -p /usr/share/webapps/
 mkdir -p /var/www/html
+mv /nginx.conf /etc/nginx
 
 # install phpMyAdmin
-mkdir -p /usr/share/webapps/
 
-cd /usr/share/webapps
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.7/phpMyAdmin-4.9.7-all-languages.tar.gz
 
-tar zxvf phpMyAdmin-5.0.2-all-languages.tar.gz
-rm phpMyAdmin-5.0.2-all-languages.tar.gz
-mv phpMyAdmin-5.0.2-all-languages phpmyadmin
+tar -xvf phpMyAdmin*
+rm -rf *.tar.gz 
+mv phpMyAdmin* /usr/share/webapps/phpmyadmin
 
 chmod -R 777 /usr/share/webapps/
 
 ln -s /usr/share/webapps/phpmyadmin/ /var/www/html/phpmyadmin
-mv config.inc.php /var/www/html/phpmyadmin/
-rm -rf /var/www/html/phpmyadmin/config-sample.inc.php
 
-echo "Run php-fpm7 & nginx -g 'daemon off;'"
+mv /config.inc.php /var/www/html/phpmyadmin/
+
 php-fpm7 && nginx -g 'daemon off;'
